6a61b36c95d12250a9fd2c92c78bbf6deb429b4d
JoseLeMalin <seb_488@hotmail.fr>
2023-08-07 09:44:56 +0200

    Removed tracking of dist files in server repo

--- packages/server/dist/app.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateCatDto } from "./data-transfer-objects/test-SHE-dto";
-export declare class AppService {
-    getHello(): string;
-    postHello(createCatDto: CreateCatDto): string;
-}
